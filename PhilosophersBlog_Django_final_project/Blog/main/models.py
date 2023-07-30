369782b9df304dfe10b5a58134e6d6321bd2a066
boris <escaper.0307@gmail.com>
2023-07-30 21:06:59 +0300

    Added Post, Category, Comment and Like models

--- PhilosophersBlog_Django_final_project/Blog/main/models.py
+++ PhilosophersBlog_Django_final_project/Blog/main/models.py
@@ -1,3 +1,83 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+UserModel = get_user_model()
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    title = models.CharField(
+        max_length=40,
+        unique=True,
+    )
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=200,
+    )
+
+    content = models.TextField()
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+    )
+
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE,
+    )
+
+    created_on = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    edited_on = models.DateTimeField(
+        auto_now=True,
+    )
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Comment(models.Model):
+    class Meta:
+        ordering = ['created_on']
+
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE,
+    )
+
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+    )
+
+    content = models.TextField()
+
+    created_on = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return f'{self.content[:30]}'
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE,
+    )
+
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+    )
+
