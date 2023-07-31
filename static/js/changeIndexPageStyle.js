b8c30bc2758c5e2f7734031f3b1744eabe61bd91
boris <escaper.0307@gmail.com>
2023-08-01 01:11:29 +0300

    Added Profile, Posts and Post-details pages style changes

--- static/js/changeIndexPageStyle.js
+++ static/js/changeIndexPageStyle.js
@@ -1,11 +1,24 @@
 const posts = document.querySelectorAll('.post');
+const postsContainer = document.querySelector('.post-container')
+const pageContent = document.querySelector(".page-content")
+const categoriesWidget = document.querySelector(".widget")
 const numPosts = posts.length;
 if (numPosts === 1) {
     posts[0].style.maxWidth = 'none';
     posts[0].style.flexBasis = 'auto';
+    posts[0].style.maxWidth = '600px';
+    posts[0].style.maxheight = '300px';
+    categoriesWidget.remove()
+    pageContent.style.margin = '0';
+    pageContent.style.alignItems = 'center';
+
+
+}
+
+if (numPosts === 2) {
+    postsContainer.style.flexWrap = 'nowrap';
 }
 
 if (numPosts < 1) {
-    const categoriesWidget = document.querySelector(".post-content");
-    categoriesWidget.remove()
+    pageContent.remove();
 }
