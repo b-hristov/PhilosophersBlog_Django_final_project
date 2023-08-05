69808723b2c5d99d8ae9a548329fdf6b11158db1
boris <escaper.0307@gmail.com>
2023-08-05 23:18:00 +0300

    Added posts to the admin panel

--- PhilosophersBlog_Django_final_project/Blog/main/admin.py
+++ PhilosophersBlog_Django_final_project/Blog/main/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from PhilosophersBlog_Django_final_project.Blog.main.models import Category
+from PhilosophersBlog_Django_final_project.Blog.main.models import Category, Post
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
     list_display = ('title',)
