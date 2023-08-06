2720e02ac75a34afa6a6f1ebe6e26e87e2307bbf
boris <escaper.0307@gmail.com>
2023-08-06 19:29:06 +0300

    Added 'Edit post' and 'Delete post' functionalities

--- PhilosophersBlog_Django_final_project/Blog/main/urls.py
+++ PhilosophersBlog_Django_final_project/Blog/main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from .views import IndexView, PostDetailsView, CreatePostView
+from .views import IndexView, PostDetailsView, CreatePostView, EditPostView, DeletePostView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('post/<int:pk>/', PostDetailsView.as_view(), name='post_details'),
     path('create-post/', CreatePostView.as_view(), name='create_post'),
+    path('post/<int:pk>/edit/', EditPostView.as_view(), name='edit_post'),
+    path('post/<int:pk>/delete/', DeletePostView.as_view(), name='delete_post'),
 ]
--- PhilosophersBlog_Django_final_project/Blog/main/views.py
+++ PhilosophersBlog_Django_final_project/Blog/main/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from PhilosophersBlog_Django_final_project.Blog.main.forms import CreatePostForm
 from PhilosophersBlog_Django_final_project.Blog.main.models import Post, Category
@@ -19,7 +20,7 @@ class IndexView(ListView):
         return context
 
 
-class CreatePostView(CreateView):
+class CreatePostView(LoginRequiredMixin, CreateView):
     template_name = 'post/create-post.html'
     form_class = CreatePostForm
     success_url = reverse_lazy('index')  # to be changed later to reverse_lazy('my_posts')
@@ -30,9 +31,22 @@ class CreatePostView(CreateView):
         return kwargs
 
 
-class PostDetailsView(DetailView):
+class PostDetailsView(LoginRequiredMixin, DetailView):
     template_name = 'post/post-details.html'
     model = Post
     context_object_name = 'post'
 
 
+class EditPostView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'post/edit-post.html'
+    fields = ['title', 'content', 'category']
+
+    def get_success_url(self):
+        return reverse_lazy('post_details', kwargs={'pk': self.object.pk})
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'post/delete-post.html'
+    success_url = reverse_lazy('index')
